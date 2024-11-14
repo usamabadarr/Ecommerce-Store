fcbf6e6f52d1fbd31deb0d5f353288ceb576a7d0
AdminChatter <xiangzhenwuyu@gmail.com>
2024-11-14 14:46:27 -0500

    small fix in header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -12,12 +12,9 @@ const Header = () => {
             <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
                 <Link className="text-dark" to="/">
                     <h1 className="m-0" style={{ fontSize: '3rem' }}>
-                        Tech Friends
+                        Ecommerce-Store
                     </h1>
                 </Link>
-                <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
-                    Meet your new programming pals.
-                </p>
                 <div>
                     {Auth.loggedIn() ? (
                         <>
