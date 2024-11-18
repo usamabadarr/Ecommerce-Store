c1c77960ea95bc3e1da775f1825763a663eb850f
RealChillyPalmer <highvibes.dev@gmail.com>
2024-11-18 18:01:43 -0500

    Folder Structure for Item/Department database

---
+++ server/src/models/Department.ts
@@ -0,0 +1,47 @@
+import { Schema, model, Document, ObjectId } from 'mongoose';
+
+
+
+interface IDepartment extends Document {
+    name: string;
+    image: string;
+    items: ObjectId[];
+    totalStock?: number;
+    // featured?: [featuredItemSchema];
+    lastAccessed?: Date;
+};
+
+const departmentSchema = new Schema<IDepartment>(
+    {
+        name: { type: String, required: true },
+        image: { type: String, required: true },
+        items: [{ type: Schema.Types.ObjectId, ref: 'Item' }],
+        totalStock: { type: Number, required: false },
+        // featured: { type: Array, required: false },
+        lastAccessed: { type: Date, default: Date.now }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+const Department = model('Department', departmentSchema);
+
+
+export default Department
+
+// Department
+//     .create({
+//         name: 'Electronics',
+//         image: '',
+//         totalStock: 0,
+//         featured: '',
+//         lastAccessed:
+//     })
+//     .then(result => console.log('Created new department', result))
+//     .catch(err => console.log(err))
+
+
