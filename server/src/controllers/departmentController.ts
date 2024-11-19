5f679ea009ecb30fe7f2e2f831688a7bc557fa5b
RealChillyPalmer <highvibes.dev@gmail.com>
2024-11-18 20:58:09 -0500

    Model/Controllers/Routes for Items in server side

--- server/src/controllers/departmentController.ts
+++ server/src/controllers/departmentController.ts
@@ -2,7 +2,7 @@ import Department from '../models/Department.js'
 import { Request, Response } from 'express';
 
 export const getDepartments = async (_req: Request, res: Response) => {
-    try{
+    try {
         const departments = await Department.find();
         res.json(departments);
     } catch (err) {
@@ -13,14 +13,14 @@ export const getDepartments = async (_req: Request, res: Response) => {
 export const getSingleDepartment = async (req: Request, res: Response) => {
     try {
         const department = await Department.findOne({ _id: req.params.departmentId })
-        .select('-__v')
-        .populate('items');
+            .select('-__v')
+            .populate('items');
 
-    if (!department) {
-        res.status(404).json({ message: 'No Department with that ID' })
-    } else {
-        res.json(department);
-    }
+        if (!department) {
+            res.status(404).json({ message: 'No Department with that ID' })
+        } else {
+            res.json(department);
+        }
     } catch (err) {
         res.status(500).json(err);
     }
