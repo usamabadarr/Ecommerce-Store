6e056783541679b646c785ef70862080670f8bbf
AdminChatter <xiangzhenwuyu@gmail.com>
2024-11-18 20:08:33 -0500

    change to comment current

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,24 +6,23 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-  proxy: {
-    // We have to set up our Vite client-side development server to proxy API requests to our server-side Node server at port 3001 in order to hit our GraphQL application.
-    '/graphql': {
-      target: 'http://localhost:3001',
-      changeOrigin: true,
-      secure: false,
-    },
-    '/api': {
-      target: 'http://localhost:3001',
-      changeOrigin: true,
-      secure: false,
-    },
-    '/auth': {
-      target: 'http://localhost:3001',
-      changeOrigin: true,
-      secure: false
-    },
-  },
+  // proxy: {
+  //   '/graphql': {
+  //     target: 'http://localhost:3001',
+  //     changeOrigin: true,
+  //     secure: false,
+  //   },
+  //   '/api': {
+  //     target: 'http://localhost:3001',
+  //     changeOrigin: true,
+  //     secure: false,
+  //   },
+  //   '/auth': {
+  //     target: 'http://localhost:3001',
+  //     changeOrigin: true,
+  //     secure: false
+  //   },
+  // },
 },
   test: {
     globals: true,
