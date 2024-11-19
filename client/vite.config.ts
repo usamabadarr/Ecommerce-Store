58746a5976a3bbc65b4a637005949884224aff80
AdminChatter <xiangzhenwuyu@gmail.com>
2024-11-18 20:05:49 -0500

    add proxy in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,30 @@
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     port: 3000,
     open: true,
+  proxy: {
+    // We have to set up our Vite client-side development server to proxy API requests to our server-side Node server at port 3001 in order to hit our GraphQL application.
+    '/graphql': {
+      target: 'http://localhost:3001',
+      changeOrigin: true,
+      secure: false,
+    },
+    '/api': {
+      target: 'http://localhost:3001',
+      changeOrigin: true,
+      secure: false,
+    },
+    '/auth': {
+      target: 'http://localhost:3001',
+      changeOrigin: true,
+      secure: false
+    },
   },
+},
   test: {
     globals: true,
     environment: 'happy-dom',
