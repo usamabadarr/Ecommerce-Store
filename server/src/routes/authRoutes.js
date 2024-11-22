f97d13f37ac31a5ae3ff148d8202ee88d3e15fd8
Ricardo Philippe <philippericardo26@gmail.com>
2024-11-21 16:36:04 -0800

    updated files

--- server/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var authController_1 = require("../controllers/authController");
-var router = (0, express_1.Router)();
-router.post('/login', authController_1.login);
-router.post('/register', authController_1.register);
-exports.default = router;
--- server/src/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-var PORT = process.env.PORT || 3000;
-app_1.default.listen(PORT, function () {
-    console.log("Server running on http://localhost:".concat(PORT));
-});
