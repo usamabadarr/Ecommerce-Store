9a27c879dab5d10e6a3871317df6d053ca5c2409
usamabadarr <usamabadarr@gmail.com>
2024-11-21 20:04:15 -0500

    added filters.cy.ts, updated keys.json, and mockProduct.json, and Home.cy.ts

--- cypress/component/Home.cy.tsx
+++ cypress/component/Home.cy.tsx
@@ -2,14 +2,31 @@ import React from 'react';
 import Home from '../../client/src/pages/Home';
 
 describe('<Home /> Component Tests', () => {
-  it('renders successfully', () => {
+  beforeEach(() => {
     cy.mount(<Home />);
+  });
+
+  it('renders successfully', () => {
     cy.contains('Welcome');
   });
 
   it('has expected elements', () => {
-    cy.mount(<Home />);
     cy.get('header').should('exist');
     cy.get('footer').should('exist');
+    cy.get('.product-list').should('exist');
+  });
+
+  it('displays correct navigation links', () => {
+    cy.get('nav a').should('have.length.at.least', 1);
+  });
+
+  it('handles click events on buttons', () => {
+    cy.get('button').contains('Learn More').click();
+    cy.url().should('include', '/about');
+  });
+
+  it('matches the snapshot', () => {
+    cy.get('body').toMatchSnapshot();
   });
 });
+
