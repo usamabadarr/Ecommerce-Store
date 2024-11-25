3dc29f7cda03d580c4078de6c7c049284a651150
usamabadarr <usamabadarr@gmail.com>
2024-11-24 20:31:15 -0500

    added authorization ID and quantity selector

--- client/src/components/ShoppingCart/ShoppingCartItem/index.tsx
+++ client/src/components/ShoppingCart/ShoppingCartItem/index.tsx
@@ -1,17 +1,43 @@
 import CartItem from "../../../interfaces/CartItem"
+import React, { useState } from "react";
 
+interface ShoppingCartItemProps {
+    item: CartItem;
+    authId: string;
+    onQuantityChange: (authId: string, itemId: string, quantity: number) => void;
+}
 
-const ShoppingCartItem = ({ item }: { item: CartItem }) => {
-    const { img, itemName, price, quantity } = item;
+const ShoppingCartItem = ({ item, authId, onQuantityChange }: ShoppingCartItemProps) => {
+    const { id, img, itemName, price } = item;
+    const [quantity, setQuantity] = useState(item.quantity);
+
+    const handleIncrement = () => {
+        const newQuantity = quantity + 1;
+        setQuantity(newQuantity);
+        onQuantityChange(authId, id, newQuantity);
+    };
+
+    const handleDecrement = () => {
+        if (quantity > 1) {
+            const newQuantity = quantity - 1;
+            setQuantity(newQuantity);
+            onQuantityChange(authId, id, newQuantity);
+        }
+    };
 
     return (
         <li>
             <img src={img} alt={itemName} />
+            <h3>{itemName}</h3>
             <p>Price: ${price}</p>
-            <p>Quantity: {quantity}</p>
+            <div>
+            <button onClick={handleDecrement}>-</button>
+                <span>{quantity}</span>
+                <button onClick={handleIncrement}>+</button>
+            </div>
+            {/* <p>Quantity: {quantity}</p> */}
             <p>Subtotal: ${price * quantity}</p>
         </li>
     );
 }
 export default ShoppingCartItem;
-
